114ceecc3b633ddcef5090d86a16d76a79e05d5b
Guvenc Terzierol <guvencterzierol@Guvencs-Laptop.local>
2024-01-02 03:35:08 +0300

    changed data fetch type because of vercel deployment failure

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 import Reservation from "@/components/modules/Reservation";
+import { Flights } from "@/types/types";
+import { useEffect, useState } from "react";
 
-export default async function Main() {
-  const data = await fetch("http://127.0.0.1:3000/api/main", {
-    method: "GET",
-  });
-  const flightData = await data.json();
+export default function Main() {
+  const [flightData, setFlightData] = useState<Flights>();
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/api/main", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data): void => {
+        setFlightData(JSON.parse(data));
+      });
+  }, []);
   return (
     <main className="mx-auto">
-      <Reservation flightData={JSON.parse(flightData)} />
+      {flightData ? <Reservation flightData={flightData} /> : null}
     </main>
   );
 }
